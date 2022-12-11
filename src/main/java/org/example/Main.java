ed4f4f2f9af0a8880678d3e85309564b1284a263
Ivan Baklanov <ibaklanov99@gmail.com>
2022-12-11 16:24:35 +0200

    Created custom type of context which allow to setup beans with property file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import org.example.api.Developer;
+import org.example.infra.PropertyFileApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("settings.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("settings.xml");
+//        context.getBean(Developer.class).speak();
+        PropertyFileApplicationContext context = new PropertyFileApplicationContext("context.properties");
         context.getBean(Developer.class).speak();
     }
 }
---
+++ src/main/java/org/example/api/JavaScriptDeveloper.java
@@ -0,0 +1,9 @@
+package org.example.api;
+
+public class JavaScriptDeveloper implements Developer{
+
+    @Override
+    public void speak() {
+        System.out.println("JavaScript is immortal");
+    }
+}
